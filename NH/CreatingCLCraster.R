17469e886f571e48054024c74b99bb6f62c620ad
WITTISCHE Julian <jwittische@hisnat.local>
2022-01-21 15:53:40 +0100

    fffffff

---
+++ NH/CreatingCLCraster.R
@@ -0,0 +1,6 @@
+# Libraries
+library("raster")
+library("sf")
+
+CLC18vec <- st_read("C:/Users/jwittische/Desktop/Projects/CLC2018/DATA/U2018_CLC2018_V2020_20u1.gpkg",
+                    query="select * from 'U2018_CLC2018_V2020_20u1.gpkg' where   )
