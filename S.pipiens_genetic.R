55f147378b74c7fefb1db0ef0e0e3074f07755d9
WITTISCHE Julian <jwittische@hisnat.local>
2022-03-16 13:11:17 +0100

    ggggggg

--- S.pipiens_genetic.R
+++ S.pipiens_genetic.R
@@ -135,24 +135,115 @@ col <- funky(5)
 s.class(PCA$li, pop(SP_genind), xax=1, yax=3, col=transp(col,.6), axesell=FALSE, 
         cstar=0, cpoint=3, grid=FALSE)
 
+colorplot(PCA$li, PCA$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+title("PCA based on microsatellite genotypes \naxes 1-2")
+abline(v=0,h=0,col="grey", lty=2)
+
+colorplot(PCA$li[c(1,3)], PCA$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 3")
+title("PCA - S. pipiens \naxes 1-3")
+abline(v=0,h=0,col="grey", lty=2)
+
 # Weird vertical pattern let's test temporal
 
 dates <- rownames(SP_genind@tab)
-dates <- gsub("_[^_]*$|^[^_]*_", "", dates, perl=T)
+#dates <- gsub("_[^_]*$|^[^_]*_", "", dates, perl=T)
+dates <- sub("_[[:alpha:]]", "", dates)
+dates <- sub("_[[:alpha:]]", "", dates)
+# I have no idea where these errors originated but they were not present in the original metadata
+dates[c(1172, 1223, 1225, 1226, 1484, 1764)] <- c("LUX_78_1_26-07-21",
+                                               "LUX_220_1_10-07-21",
+                                               "LUX_220_1_25-07-21",
+                                               "LUX_220_1_28-07-21",
+                                               "CO_217_1_07-06-21",
+                                               "CO_36_1_31-07-21")
 library(stringr)
 dates <- str_sub(dates, start= -8)
 dates <- as.Date(dates, tryFormats = c("%d-%m-%Y"))
 class(dates)
 dates <- as.numeric(format(dates, "%j"))
 hist(dates)
+sum(is.na(dates))
+cor(PCA$li$Axis1, dates)
+cor(PCA$li$Axis2, dates)
 
-colorplot(PCA$li, PCA$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+colorplot(PCA$li, data.frame(dates, rep(0,length(dates)), rep(0,length(dates))), transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
 title("PCA - S. pipiens \naxes 1-2")
 abline(v=0,h=0,col="grey", lty=2)
 
-colorplot(PCA$li[c(1,3)], PCA$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 3")
-title("PCA - S. pipiens \naxes 1-3")
+colorplot(PCA$li, data.frame(dates, dates, dates), transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+title("PCA - S. pipiens \naxes 1-2")
 abline(v=0,h=0,col="grey", lty=2)
+
+###### Check linkage disequilibrium
+
+poppr::ia(SP_genind, sample=199)
+# There is statistically significant association among the markers but the correlation is very low.
+LD.pair <- poppr::pair.ia(SP_genind)
+LD.pair
+# /!\ HIGH /!\ between Spp141 and Spp051 ! Those two are also somewhat in HWE.
+
+# What if we get rid of Spp141 (LD and high Fis)
+SP_genind_noSpp141 <- SP_genind[loc=c("Spp010", "Spp053", "Spp080", "Spp142",
+                                "Spp231", "Spp273", "Spp476", "Spp051",
+                                "Spp108", "Spp313", "Spp360", "Spp391",
+                                "Spp416"), drop=TRUE]
+
+PCAdf_noSpp141 <- tab(SP_genind_noSpp141, freq = TRUE, NA.method = "mean")
+
+PCA_noSpp141  <- dudi.pca(PCAdf_noSpp141 , scale = FALSE, scannf = FALSE, nf = 3)
+
+colorplot(PCA_noSpp141$li, PCA_noSpp141$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+title("PCA based on microsatellite genotypes (without Spp141) \naxes 1-2")
+abline(v=0,h=0,col="grey", lty=2)
+
+colorplot(PCA_noSpp141$li[c(1,3)], PCA_noSpp141$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 3")
+title("PCA based on microsatellite genotypes (without Spp141) \naxes 1-3")
+abline(v=0,h=0,col="grey", lty=2)
+
+###### Check null alleles
+
+library(PopGenReport)
+Null.alleles <- PopGenReport::null.all(SP_genind)
+
+{cat(" summary1 (Chakraborty et al. 1994):", "\n")
+  round(Null.alleles$null.allele.freq$summary1, 2)}
+
+{cat("summary2 (Brookfield et al. 1996):", "\n")
+  round(Null.alleles$null.allele.freq$summary2, 2)}
+
+# We cannot exclude that Spp142, Spp051, Spp108, Spp141, Spp313, Spp360 and Spp416 bear null alleles
+# Spp141 is the highest here, again
+# https://doi.org/10.1093/molbev/msl191 suggests we could keep Spp108 and Spp360
+# https://dx.doi.org/10.7717%2Fpeerj.3188 warns about the relationship between
+# null alleles and genetic structure
+# NOTE: Spp142, Spp051, Spp108, Spp141, Spp360 were somewhat in HWE
+# False positive possible explanations: incomplete genotypes, genetic structure,
+# breeding patterns, differences in allele sizes, and unique alleles that are
+# fixed or nearly fixed locally
+
+##### PCA per study area (without Spp141)
+
+# CO
+PCAdf_CO <- tab(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"], freq = TRUE, NA.method = "mean")
+PCA_noSpp141_CO  <- dudi.pca(PCAdf_CO , scale = FALSE, scannf = FALSE, nf = 3)
+
+colorplot(PCA_noSpp141_CO$li, PCA_noSpp141_CO$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+title("PCA based on microsatellite genotypes (without Spp141) \naxes 1-2")
+abline(v=0,h=0,col="grey", lty=2)
+
+colorplot(as.data.frame(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"]@other$xy),
+          PCA_noSpp141_CO$li, transp=TRUE, cex=3)
+
+#SWLU
+PCAdf_SWLU <- tab(SP_genind_noSpp141[SP_genind_noSpp141@pop=="SW"|SP_genind_noSpp141@pop=="LU"], freq = TRUE, NA.method = "mean")
+PCA_noSpp141_SWLU  <- dudi.pca(PCAdf_SWLU , scale = FALSE, scannf = FALSE, nf = 3)
+
+colorplot(PCA_noSpp141_SWLU$li, PCA_noSpp141_SWLU$li, transp=TRUE, cex=3, xlab="PC 1", ylab="PC 2")
+title("PCA based on microsatellite genotypes (without Spp141) \naxes 1-2")
+abline(v=0,h=0,col="grey", lty=2)
+
+### Integrity check
+plot(SP_genind_noSpp141@other$xy, col=SP_genind_noSpp141@pop)
 
 ### CA
 obj <- genind2genpop(SP_genind)
@@ -165,15 +256,15 @@ add.scatter.eig(ca1$eig,nf=3,xax=1,yax=2,posi="bottomright")
 
 ### DAPC
 # NOTE: we should do this temporally too
-grp <- find.clusters(SP_genind, max.n.clust=40)
-100
+grp <- find.clusters(SP_genind_noSpp141, max.n.clust=40)
+1000
 3
 
-dapc1 <- dapc(SP_genind, grp$grp)
+dapc1 <- dapc(SP_genind_noSpp141, grp$grp)
 50
 2
 
-plot(SP_genind@other$xy, col=dapc1$grp)
+plot(SP_genind_noSpp141@other$xy, col=dapc1$grp)
 
 scatter(dapc1)
 
@@ -250,11 +341,11 @@ assignplot(dapc1, subset=1:50)
 compoplot(dapc1, subset=1:50, posi="bottomright",txt.leg=paste("Cluster", 1:6), lab="",ncol=2, xlab="individuals")
 
 # CO
-grp_CO <- find.clusters(SP_genind[SP_genind@pop=="CO"], max.n.clust=40)
+grp_CO <- find.clusters(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"], max.n.clust=40)
 1000
 7
 
-dapc_CO <- dapc(SP_genind[SP_genind@pop=="CO"], grp_CO$grp)
+dapc_CO <- dapc(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"], grp_CO$grp)
 40
 6
 
@@ -289,6 +380,12 @@ round(head(dapc1$posterior),3)
 summary(dapc1)
 assignplot(dapc1, subset=1:50)
 compoplot(dapc1, subset=1:50, posi="bottomright",txt.leg=paste("Cluster", 1:6), lab="",ncol=2, xlab="individuals")
+
+plot(as.data.frame(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"]@other$xy),
+          col=dapc_CO$grp, cex=1, asp=1, pch=19)
+geo <- as.data.frame(SP_genind_noSpp141[SP_genind_noSpp141@pop=="CO"]@other$xy)
+
+poppr::amova(SP_genind_noSpp141, method="pegas")
 
 ################################################################################
 ############### Spatial analyses
