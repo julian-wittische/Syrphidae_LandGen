37b121e9d5ca60758214052e203460efe60c0bea
WITTISCHE Julian <jwittische@hisnat.local>
2022-11-18 18:06:53 +0100

    rthhsrth

--- Code/2Genepop.R
+++ Code/2Genepop.R
@@ -5,9 +5,11 @@
 writeGenPop(SP, "Data/SPno141.gen",
             comment="Coordinates are in the file SPgeo.txt")
 
-write.table(as.data.frame(SP@other$xy), "Data/SPgeo.txt", header=TRUE)
+write.table(as.data.frame(SP@other$xy), "Data/SPgeo.txt",
+            col.names=FALSE, row.names = FALSE)
 
 writeGenPop(MF, "Data/MF.gen",
             comment="Coordinates are in the file MFgeo.txt")
 
-write.table(as.data.frame(MF@other$xy), "Data/MFgeo.txt", header=TRUE)
+write.table(as.data.frame(MF@other$xy), "Data/MFgeo.txt",
+            col.names=FALSE, row.names = FALSE)
