2c7665b81ec069caf3c37b19120a60ebc12dcc29
WITTISCHE Julian <jwittische@hisnat.local>
2022-12-23 17:43:08 +0100

    gggggggg

--- Code/Fieldwork/Fig1.R
+++ Code/Fieldwork/Fig1.R
@@ -1,29 +1,21 @@
-library(dplyr)
-library(latticeExtra)
-library(mapview)
-library(raster)
-library(rasterVis)
-library(RColorBrewer)
-library(reshape2)
-library(rgeos)
-library(RgoogleMaps)
-library(sf)
-library(sp)
-library(leaflet)
-library(htmlwidgets)
-library(googledrive)
-library(googlesheets4)
-library(ggplot2)
-library(maps)
-library(terra)
+################################################################################
+############################ Maps of hoverfly samples ##########################
+################################################################################
+# Project lead: Julian Wittische
+# December 2022
 
+#-------------------------------------------------------------------------------
+############################### Loading packages ###############################
+#-------------------------------------------------------------------------------
 
+source("C:/Users/jwittische/Desktop/Projects/EnvironmentalData/CLC2018/LoadingCLC2018.R")
 source("Code/GenDataPrep.R")
 
-cologne <- raster("Data/ascii/cologne_in.asc")
-crs(cologne) <- CRS('+init=EPSG:2169')
+crs(SP_genind_LUX@other$xy) <- "EPSG:3035"
+crs(SPproj@other$xy)
+
 SPproj <- SP
-SPproj@other$xy <- spTransform(SPproj@other$xy, CRS(SRS_string = "EPSG:2169"))
+SPproj@other$xy <- spTransform(SPproj@other$xy, CRS(SRS_string = "EPSG:3035"))
 MFproj <- MF
 MFproj@other$xy <- spTransform(MFproj@other$xy, CRS(SRS_string = "EPSG:2169"))
 
@@ -46,4 +38,19 @@ points(MFproj@other$xy, col="black", pch=25, bg="black", cex=1)
 
 north(xy=NULL, type=2, label="N", angle=0, head=0.1, xpd=TRUE)
 
-
+# # Legacy from older code, I am not sure what is really needed beyond sf
+# library(dplyr)
+# library(latticeExtra)
+# library(mapview)
+# library(RColorBrewer)
+# library(reshape2)
+# library(rgeos)
+# library(RgoogleMaps)
+# library(sp)
+# library(leaflet)
+# library(htmlwidgets)
+# library(googledrive)
+# library(googlesheets4)
+# library(ggplot2)
+# library(maps)
+# library(terra)
